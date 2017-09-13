cd992c8d7f9db3165e5a9ded92bc9aeb9a6a1922
appreciated <goebeljohannes@gmx.net>
2017-09-13 14:27:18 +0200

    - added demo which allows to switch between the available variations - fixes Issue #1

--- app-layout-demo/src/main/java/com/github/appreciated/demo/DemoUI.java
+++ app-layout-demo/src/main/java/com/github/appreciated/demo/DemoUI.java
@@ -5,6 +5,7 @@ import com.github.appreciated.builder.NavigationDrawerBuilder;
 import com.github.appreciated.demo.views.View1;
 import com.github.appreciated.demo.views.View2;
 import com.github.appreciated.demo.views.View3;
+import com.github.appreciated.layout.drawer.AbstractNavigationDrawer;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
@@ -12,8 +13,7 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 
 import javax.servlet.annotation.WebServlet;
@@ -30,21 +30,47 @@ public class DemoUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        setContent(NavigationDrawerBuilder.get()
-                .withVariant(DrawerVariant.LEFT_OVERLAY)
+        VerticalLayout left = new VerticalLayout();
+        left.setMargin(false);
+        VerticalLayout right = new VerticalLayout();
+        right.setWidthUndefined();
+
+        setDrawerVariant(left, DrawerVariant.LEFT);
+
+        ComboBox<DrawerVariant> variants = new ComboBox<>();
+        variants.setItems(DrawerVariant.LEFT, DrawerVariant.LEFT_OVERLAY, DrawerVariant.LEFT_RESPONSIVE, DrawerVariant.LEFT_RESPONSIVE_OVERLAY, DrawerVariant.LEFT_RESPONSIVE_OVERLAY_NOAPPBAR);
+        variants.addValueChangeListener(valueChangeEvent -> {
+            setDrawerVariant(left, valueChangeEvent.getValue());
+        });
+        variants.setValue(DrawerVariant.LEFT);
+        right.addComponent(new Panel("Variants", new VerticalLayout(variants)));
+
+        HorizontalLayout layout = new HorizontalLayout(left, right);
+        layout.setSpacing(false);
+        layout.setExpandRatio(left, 1);
+        layout.setSizeFull();
+        setContent(layout);
+    }
+
+    private void setDrawerVariant(VerticalLayout rightside, DrawerVariant variant) {
+        rightside.removeAllComponents();
+
+        AbstractNavigationDrawer drawer = NavigationDrawerBuilder.get()
+                .withVariant(variant)
                 .withTitle("App Layout Demo")
                 .withAppBarElement(getBorderlessButtonWithIcon(VaadinIcons.ELLIPSIS_DOTS_V))
-                .withDefaultNavigationView(View2.class)
-                .withNavigationElement("Home", VaadinIcons.HOME, View1.class)
-                .withNavigationElement("Charts", VaadinIcons.SPLINE_CHART, new View2())
-                .withNavigationElement("Contact", VaadinIcons.CONNECT, View3.class)
+                .withDefaultNavigationView(View1.class)
+                .withNavigationElement("Home", VaadinIcons.HOME, View2.class)
+                .withNavigationElement("Charts", VaadinIcons.SPLINE_CHART, View3.class)
+                .withNavigationElement("Contact", VaadinIcons.CONNECT, View1.class)
                 .withSection("More")
-                .withNavigationElement("More", VaadinIcons.PLUS, View1.class)
-                .withNavigationElement("Menu", VaadinIcons.MENU, View2.class)
-                .withNavigationElement("Elements", VaadinIcons.LIST, View3.class)
+                .withNavigationElement("More", VaadinIcons.PLUS, View2.class)
+                .withNavigationElement("Menu", VaadinIcons.MENU, View3.class)
+                .withNavigationElement("Elements", VaadinIcons.LIST, View1.class)
                 .withSection("Settings")
-                .withNavigationElement("Preferences", VaadinIcons.COG, View1.class)
-                .build());
+                .withNavigationElement("Preferences", VaadinIcons.COG, View2.class)
+                .build();
+        rightside.addComponent(drawer);
     }
 
     private Button getBorderlessButtonWithIcon(VaadinIcons icon) {
