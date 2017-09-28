dbf97c1b7ea7daeda52fddf45bb2f6cc3616d7ad
appreciated <goebeljohannes@gmx.net>
2017-09-28 02:24:23 +0200

    0.9.1  - header improvements

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/MenuHeader.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/MenuHeader.java
@@ -2,6 +2,7 @@ package com.github.appreciated.app.layout.component;
 
 import com.vaadin.server.Resource;
 import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -11,14 +12,16 @@ import static com.github.appreciated.app.layout.Styles.APP_LAYOUT_MENU_BAR_ELEME
 public class MenuHeader extends VerticalLayout {
     public MenuHeader(String title, String subtitle, Resource resource) {
         Label name = new Label(title);
-        name.addStyleName(ValoTheme.LABEL_H4);
         Label description = new Label(subtitle);
         description.addStyleName(ValoTheme.LABEL_SMALL);
-        RoundResourceButton resourceButton = new RoundResourceButton(resource, "75px", "75px");
-        addStyleName(APP_LAYOUT_MENU_BAR_ELEMENT);
-        setMargin(false);
-        setSpacing(false);
-        setMargin(new MarginInfo(true, false));
-        addComponents(resourceButton, name, description);
+        Image image = new Image(null, resource);
+        image.setWidth("75px");
+        image.setHeight("75px");
+        image.addStyleName(ValoTheme.BUTTON_ICON_ONLY);
+        this.addStyleName(APP_LAYOUT_MENU_BAR_ELEMENT);
+        this.setMargin(false);
+        this.setSpacing(false);
+        this.setMargin(new MarginInfo(true, false));
+        this.addComponents(image, name, description);
     }
 }
