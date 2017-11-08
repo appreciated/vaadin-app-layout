8dda4a4e38c2071ad7a95fc4a8e776f96d74b554
appreciated <goebeljohannes@gmx.net>
2017-11-08 15:30:27 +0100

    0.9.12  - update the build.sh

--- app-layout-addon/src/main/polymer/build.sh
+++ app-layout-addon/src/main/polymer/build.sh
@@ -1,2 +1,4 @@
+npm install -g polymer-cli
 polymer install
-polymer build
+polymer build --js-compile
+echo "you have successfully build the JS dependencies for this project now you only need to update src/main/webapp/VAADIN/bower_components with the freshly built one in build/default/bower_components"
