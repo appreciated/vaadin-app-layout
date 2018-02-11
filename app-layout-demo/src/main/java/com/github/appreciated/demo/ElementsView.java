3c25ad82f62eee7993ee82fa3e3716895d52a74e
Deya Eddin <system@yanax.com>
2018-02-11 09:19:50 +0300

    - Remove vaadin elements from the app-layout package and add them to the demo app for show case.

---
+++ app-layout-demo/src/main/java/com/github/appreciated/demo/ElementsView.java
@@ -0,0 +1,9 @@
+package com.github.appreciated.demo;
+
+import com.vaadin.annotations.HtmlImport;
+import com.vaadin.navigator.View;
+import com.vaadin.ui.VerticalLayout;
+
+@HtmlImport("src/login/bakery-login.html")
+public class ElementsView extends VerticalLayout implements View {
+}
