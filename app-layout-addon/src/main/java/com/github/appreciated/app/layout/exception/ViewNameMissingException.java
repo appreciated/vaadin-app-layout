e51954a80a9cd9699572bbd6b06eb37f717438db
Johannes Goebel <j.goebel@prisma-computer.de>
2018-04-04 17:36:14 +0200

    0.9.22
    - updated error message

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/exception/ViewNameMissingException.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/exception/ViewNameMissingException.java
@@ -5,7 +5,7 @@ import com.vaadin.navigator.View;
 public class ViewNameMissingException extends RuntimeException {
     public ViewNameMissingException(Class<? extends View> view) {
         super("The class " + view.getName() + " seems to miss the required view name to add it to the Navigator. " +
-                "To add it either add the Annotation '@NavigatorViewName' " +
-                "or by calling the method '<AppLayoutBuilder>::add()' properly!");
+                "To fix this issue either add the Annotation '@NavigatorViewName' to the View " + view.getName() +
+                "or add the View with a non null value '<AppLayoutBuilder>::add()' to the AppLayout!");
     }
 }
