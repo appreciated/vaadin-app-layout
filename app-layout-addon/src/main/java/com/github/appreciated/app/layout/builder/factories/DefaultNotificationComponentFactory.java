0b89e60634123d6db0a1eae6869dbe110c37515e
appreciated <GoebelJohannes@gmx.net>
2018-05-26 11:38:52 +0200

    - added paper-ripple - improved notification style

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/factories/DefaultNotificationComponentFactory.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/factories/DefaultNotificationComponentFactory.java
@@ -1,48 +1,17 @@
 package com.github.appreciated.app.layout.builder.factories;
 
-import com.github.appreciated.app.layout.builder.design.Styles;
 import com.github.appreciated.app.layout.builder.entities.DefaultNotification;
 import com.github.appreciated.app.layout.builder.entities.NotificationHolder;
 import com.github.appreciated.app.layout.builder.interfaces.PairComponentFactory;
-import com.github.appreciated.app.layout.component.RoundImage;
-import com.github.appreciated.app.layout.webcomponents.papercard.PaperCard;
+import com.github.appreciated.app.layout.component.NotificationView;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public class DefaultNotificationComponentFactory implements PairComponentFactory<NotificationHolder, DefaultNotification> {
     @Override
     public Component getComponent(NotificationHolder holder, DefaultNotification info) {
-        PaperCard card = new PaperCard();
-        card.getElement().getStyle().set("width", "100%").set("color", "#000000");
-
-        Label timeAgo = new Label(info.getTimeAgo());
-        timeAgo.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_TIME);
-
-        Label title = new Label(info.getTitle());
-        title.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_TITLE);
-
-        Label description = new Label(info.getDescription());
-        description.setWidth("100%");
-        description.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_DESCRIPTION);
-        HorizontalLayout descriptionWrapper = new HorizontalLayout(description);
-
-        descriptionWrapper.setWidth("100%");
-        if (info.getImage() != null) {
-            RoundImage image = new RoundImage(info.getImage());
-            descriptionWrapper.add(image);
-        }
-       /* if (!info.isUnread()) {
-            card.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_READ);
-        }*/
-        card.add(new HorizontalLayout(new HorizontalLayout(title, timeAgo)));
-        card.add(descriptionWrapper);
-        /*wrapper.addLayoutClickListener(layoutClickEvent -> {
+        return new NotificationView(info, domEvent -> {
             info.setUnread(false);
             holder.onNotificationClicked(info);
-            wrapper.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_READ);
-        });*/
-        card.getElement().getClassList().add(info.getStyle());
-        return card;
+        });
     }
 }
---
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/NotificationView.java
@@ -0,0 +1,66 @@
+package com.github.appreciated.app.layout.component;
+
+import com.github.appreciated.app.layout.builder.entities.DefaultNotification;
+import com.github.appreciated.app.layout.webcomponents.papercard.PaperCard;
+import com.github.appreciated.app.layout.webcomponents.paperripple.PaperRipple;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.dom.DomEventListener;
+
+public class NotificationView extends PaperCard {
+
+    public NotificationView(DefaultNotification info) {
+        this(info, null);
+
+    }
+
+    public NotificationView(DefaultNotification info, DomEventListener clickEvent) {
+        getElement().getStyle().set("width", "100%").set("color", "#000000");
+
+        Label title = new Label(info.getTitle());
+        title.getElement().getStyle()
+                .set("font-size", "15px")
+                .set("font-weight", "500");
+
+        Label dot = new Label("·");
+        dot.getElement().getStyle()
+                .set("margin-left", "5px");
+
+        Label timeAgo = new Label(info.getTimeAgo());
+        timeAgo.getElement().getStyle()
+                .set("font-size", "13px")
+                .set("margin-left", "5px")
+                .set("font-weight", "300");
+
+        Label description = new Label(info.getDescription());
+        description.setWidth("100%");
+        description.getElement().getStyle()
+                .set("font-size", "15px")
+                .set("font-weight", "400");
+
+        HorizontalLayout descriptionWrapper = new HorizontalLayout(description);
+
+        descriptionWrapper.setWidth("100%");
+        if (info.getImage() != null) {
+            RoundImage image = new RoundImage(info.getImage());
+            descriptionWrapper.add(image);
+        }
+       /* if (!info.isUnread()) {
+            card.getElement().getClassList().add(Styles.APP_BAR_NOTIFICATION_READ);
+        }*/
+        HorizontalLayout headerLine = new HorizontalLayout(title, dot, timeAgo);
+        headerLine.setSpacing(false);
+        headerLine.setAlignItems(FlexComponent.Alignment.CENTER);
+        add(headerLine);
+        add(descriptionWrapper);
+        if (clickEvent != null) {
+            getElement().addEventListener("click", clickEvent);
+
+        }
+        getElement().getClassList().add(info.getStyle());
+        if (clickEvent != null) {
+            add(new PaperRipple());
+        }
+    }
+}
