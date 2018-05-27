1d711dbbc5b94f7480b5763615eed3a86d1bc173
appreciated <GoebelJohannes@gmx.net>
2018-05-27 18:37:36 +0200

    - merged master with dev-10.x

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/interfaces/NavigationElementClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.appreciated.app.layout.builder.interfaces;
-
-import com.github.appreciated.app.layout.builder.elements.AbstractNavigationElement;
-import com.vaadin.ui.Button;
-
-public interface NavigationElementClickListener {
-    void onClick(AbstractNavigationElement element, Button.ClickEvent event);
-}
