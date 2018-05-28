59a99c73113bcefa1579cd9019cf13f35680adf5
appreciated <GoebelJohannes@gmx.net>
2018-05-28 13:58:40 +0200

    - some styling issues fixed for multiple behaviours

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/behaviour/left/LeftResponsive.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/behaviour/left/LeftResponsive.java
@@ -1,16 +1,17 @@
 package com.github.appreciated.app.layout.behaviour.left;
 
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 
 
-/**
- * Created by appreciated on 01.05.2017.
- */
-//@JavaScript("vaadin://addons/app-layout/babel-helpers.js")
-//@JavaScript("vaadin://addons/app-layout/app-layout-es5-listener.js")
-//@JavaScript("frontend://bower_components/webcomponentsjs/webcomponents-lite.js")
-
-@HtmlImport("frontend://com/github/appreciated/app/layout/behaviour/left/left-responsive.html")
+@Tag("app-layout-left-responsive")
+@HtmlImport("frontend://bower_components/vaadin-icons/vaadin-icons.html")
+@HtmlImport("frontend://bower_components/app-layout/app-drawer/app-drawer.html")
+@HtmlImport("frontend://bower_components/app-layout/app-drawer-layout/app-drawer-layout.html")
+@HtmlImport("frontend://bower_components/app-layout/app-header/app-header.html")
+@HtmlImport("frontend://bower_components/app-layout/app-header-layout/app-header-layout.html")
+@HtmlImport("frontend://bower_components/app-layout/app-toolbar/app-toolbar.html")
+@HtmlImport("frontend://com/github/appreciated/app-layout/left/left-responsive.html")
 public class LeftResponsive extends AbstractLeftAppLayoutBase {
 
     @Override
