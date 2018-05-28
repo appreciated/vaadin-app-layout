82f716f98f03d1633171504d898fef20957f1c85
appreciated <GoebelJohannes@gmx.net>
2018-05-28 14:29:19 +0200

    - some java script code refactoring - fixed left-responsive-hybrid-no-app-bar

--- app-layout-addon/src/test/java/com/github/appreciated/MainView.java
+++ app-layout-addon/src/test/java/com/github/appreciated/MainView.java
@@ -45,7 +45,7 @@ public class MainView extends AppLayoutRouterLayout {
     @Override
     public AbstractAppLayoutBuilderBase getConfiguration() {
         if (variant == null) {
-            variant = Behaviour.LEFT_HYBRID;
+            variant = Behaviour.LEFT_RESPONSIVE;
             notifications = new DefaultNotificationHolder();
             badge = new DefaultBadgeHolder();
         }
