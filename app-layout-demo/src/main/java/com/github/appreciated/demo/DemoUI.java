8eef1137ec6c2b12ce3689c3131e1f28a226e98e
Johannes Goebel <GoebelJohannes@gmx.net>
2018-06-21 11:36:40 +0200

    updated vaadin versions and demo versions

--- app-layout-demo/src/main/java/com/github/appreciated/demo/DemoUI.java
+++ app-layout-demo/src/main/java/com/github/appreciated/demo/DemoUI.java
@@ -88,7 +88,7 @@ public class DemoUI extends UI {
                 .withDefaultNavigationView(View1.class)
                 .withDesign(AppLayoutDesign.MATERIAL)
                 //.withNavigatorConsumer(navigator -> {/* Do someting with it */})
-                .add(new MenuHeader("Version 0.9.23", new ThemeResource("logo.png")), HEADER)
+                .add(new MenuHeader("Version 1.0.0", new ThemeResource("logo.png")), HEADER)
                 .addClickable("Set Behaviour HEADER", VaadinIcons.COG, clickEvent -> openModeSelector(variant), HEADER)
                 .add("Home", VaadinIcons.HOME, badge, new View1())
                 .add(SubmenuBuilder.get("My Submenu", VaadinIcons.PLUS)
