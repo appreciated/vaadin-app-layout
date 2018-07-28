83d915e0e6d6a7db93111c49976b7ce5b8e23258
Deya Eddin <system@yanax.com>
2018-07-28 18:12:22 +0300

    loading resources on test ui

--- app-layout-ui-tests/src/main/java/com/github/appreciated/app/layout/test/BasicMain.java
+++ app-layout-ui-tests/src/main/java/com/github/appreciated/app/layout/test/BasicMain.java
@@ -2,16 +2,22 @@ package com.github.appreciated.app.layout.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-public class BasicMain {
+public class BasicMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicMain.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BasicMain.class);
+    }
 }
