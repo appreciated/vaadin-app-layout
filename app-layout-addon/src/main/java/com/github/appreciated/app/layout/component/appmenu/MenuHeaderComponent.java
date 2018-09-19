d9e4eed639fc8afc4aaa2f5d73c737c807140645
Vincenzo Calabrese <vincenzo.calabrese89@hotmail.it>
2018-09-19 15:08:17 +0200

    Added getter and setter in MenuHeaderComponent and new costructor. In this way is possible retrieve the single added component and manipulate it (ex. add Listener)

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/appmenu/MenuHeaderComponent.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/appmenu/MenuHeaderComponent.java
@@ -8,22 +8,76 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
  */
 public class MenuHeaderComponent extends VerticalLayout {
 
-    public MenuHeaderComponent(String title, String subtitle, String src) {
-        setMargin(false);
-        setId("menu-header-wrapper");
-        if (src != null) {
-            add(new RoundImage(src, "56px", "56px"));
-        }
-        if (title != null) {
-            Label titleLabel = new Label(title);
-            titleLabel.setId("menu-header-title");
-            add(titleLabel);
-        }
-        if (subtitle != null) {
-            Label subtitleLabel = new Label(subtitle);
-            subtitleLabel.setId("menu-header-subtitle");
-            add(subtitleLabel);
-        }
-    }
+	private RoundImage image;
+	private Label titleLabel;
+	private Label subtitleLabel;
 
+	public MenuHeaderComponent( ){ }
+
+	public MenuHeaderComponent( String title, String subtitle, String src ){
+		if( src != null )
+			image = new RoundImage( src, "56px", "56px" );
+
+		if( title != null ){
+			titleLabel = new Label( title );
+			add( titleLabel );
+		}
+
+		if( subtitle != null ){
+			subtitleLabel = new Label( subtitle );
+			add( subtitleLabel );
+		}
+
+		init( );
+	}
+
+	public MenuHeaderComponent( Label titleLabel, Label subtitleLabel, RoundImage image ){
+		this.image = image;
+		this.titleLabel = titleLabel;
+		this.subtitleLabel = subtitleLabel;
+
+		init( );
+	}
+
+	private void init( ){
+		setMargin( false );
+		setId( "menu-header-wrapper" );
+
+		if( image != null )
+			add( image );
+
+		if( titleLabel != null ){
+			titleLabel.setId( "menu-header-title" );
+			add( titleLabel );
+		}
+
+		if( subtitleLabel != null ){
+			subtitleLabel.setId( "menu-header-subtitle" );
+			add( subtitleLabel );
+		}
+	}
+
+	public RoundImage getImage( ){
+		return image;
+	}
+
+	public void setImage( RoundImage image ){
+		this.image = image;
+	}
+
+	public Label getTitleLabel( ){
+		return titleLabel;
+	}
+
+	public void setTitleLabel( Label titleLabel ){
+		this.titleLabel = titleLabel;
+	}
+
+	public Label getSubtitleLabel( ){
+		return subtitleLabel;
+	}
+
+	public void setSubtitleLabel( Label subtitleLabel ){
+		this.subtitleLabel = subtitleLabel;
+	}
 }
