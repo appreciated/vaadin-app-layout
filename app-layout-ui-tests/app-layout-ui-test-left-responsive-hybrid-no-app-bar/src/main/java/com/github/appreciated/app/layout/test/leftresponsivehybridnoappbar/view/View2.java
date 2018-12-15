62dffbb480075669e5b7e578a92db8cbf3617dbd
appreciated <GoebelJohannes@gmx.net>
2018-12-15 18:14:26 +0100

    added responsive hybrid no app bar test

---
+++ app-layout-ui-tests/app-layout-ui-test-left-responsive-hybrid-no-app-bar/src/main/java/com/github/appreciated/app/layout/test/leftresponsivehybridnoappbar/view/View2.java
@@ -0,0 +1,14 @@
+package com.github.appreciated.app.layout.test.leftresponsivehybridnoappbar.view;
+
+import com.github.appreciated.app.layout.test.leftresponsivehybridnoappbar.LeftResponsiveHybridNoAppBarBehaviourView;
+import com.github.appreciated.app.layout.test.view.ExampleView;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "view2", layout = LeftResponsiveHybridNoAppBarBehaviourView.class)
+// an empty view name will also be the default view
+public class View2 extends ExampleView {
+    @Override
+    protected String getViewName() {
+        return getClass().getName();
+    }
+}
