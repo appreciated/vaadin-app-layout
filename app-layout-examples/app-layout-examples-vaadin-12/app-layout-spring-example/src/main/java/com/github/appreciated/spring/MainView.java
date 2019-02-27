6757ef25b4d26555d4b2e06f8e31f5d7012ab5f6
appreciated <GoebelJohannes@gmx.net>
2019-02-27 11:47:09 +0100

    - updated maven example project structure

---
+++ app-layout-examples/app-layout-examples-vaadin-12/app-layout-spring-example/src/main/java/com/github/appreciated/spring/MainView.java
@@ -0,0 +1,20 @@
+package com.github.appreciated.spring;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route
+@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired MessageBean bean) {
+        Button button = new Button("Click me",
+                e -> Notification.show(bean.getMessage()));
+        add(button);
+    }
+
+}
