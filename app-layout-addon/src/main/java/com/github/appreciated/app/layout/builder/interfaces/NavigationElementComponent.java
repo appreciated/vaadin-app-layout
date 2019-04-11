c3d5b1ca0db99070940ef4056be2a015695ed34e
Juchar <Julien.Charpenel@Gmail.com>
2019-04-11 17:01:25 +0200

    Update route similarity to take aliases into account

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/interfaces/NavigationElementComponent.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/interfaces/NavigationElementComponent.java
@@ -1,7 +1,5 @@
 package com.github.appreciated.app.layout.builder.interfaces;
 
-import com.vaadin.flow.component.Component;
-
 /**
  * This interface is for Components that contain Elements that are clickable and the interaction with it leads to route
  * changes.
@@ -9,5 +7,5 @@ import com.vaadin.flow.component.Component;
  * without the elements knowledge (f.e. loading the page with a nested url).
  */
 public interface NavigationElementComponent extends NavigationElement {
-    void register();
+
 }
