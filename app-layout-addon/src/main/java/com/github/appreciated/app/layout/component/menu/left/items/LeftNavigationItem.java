appreciated/vaadin-app-layout

commit b5e6aca69a1400ae4385005f2137584f03c38f2c

Adapt RouteSimilarity to take aliases into account (#255)

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/left/items/LeftNavigationItem.java
@@ -1,7 +1,7 @@
 package com.github.appreciated.app.layout.component.menu.left.items;
 
 import com.github.appreciated.app.layout.annotations.Caption;
-import com.github.appreciated.app.layout.builder.interfaces.NavigationElementComponent;
+import com.github.appreciated.app.layout.builder.interfaces.NavigationElement;
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElementContainer;
 import com.github.appreciated.app.layout.router.navigation.UpNavigationHelper;
 import com.vaadin.flow.component.Component;
@@ -19,7 +19,7 @@ import com.vaadin.flow.router.RouterLink;
  * {@link com.vaadin.flow.component.Component} which will usually causes a change of the View at the
  * AppLayout content view.
  */
-public class LeftNavigationItem extends LeftBadgeIconItem implements NavigationElementComponent, BeforeEnterObserver {
+public class LeftNavigationItem extends LeftBadgeIconItem implements NavigationElement, BeforeEnterObserver {
     private boolean highlight = false;
     /**
      * The caption of this menu element
@@ -46,7 +46,6 @@ public class LeftNavigationItem extends LeftBadgeIconItem implements NavigationE
         this.icon = icon;
         this.className = className;
         setRoute(UI.getCurrent().getRouter(), className);
-        UpNavigationHelper.registerNavigationRoute(className);
         setHighlightCondition((routerLink, event) -> UpNavigationHelper.shouldHighlight(className, event));
         HighlightAction<RouterLink> action = getHighlightAction();
 
@@ -88,10 +87,5 @@ public class LeftNavigationItem extends LeftBadgeIconItem implements NavigationE
         if (parent != null && highlight) {
             parent.setActiveNavigationElement(null);
         }
-    }
-
-    @Override
-    public void register() {
-        UpNavigationHelper.registerNavigationRoute(className);
     }
 }

app-layout-addon/src/main/java/com/github/appreciated/app/layout/router/navigation/UpNavigationHelper.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.router.RouterLink;
 
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,8 +16,6 @@ import java.util.Optional;
  * and helps in finding possible parent views
  */
 public class UpNavigationHelper {
-
-    private HashMap<Class<? extends Component>, String> registeredRoutes = new HashMap();
 
     private UpNavigationHelper() {
 
@@ -47,36 +46,13 @@ public class UpNavigationHelper {
     }
 
     public static boolean shouldHighlight(Class<? extends Component> className, AfterNavigationEvent event) {
-        HashMap<Class<? extends Component>, String> routes = getUpNavigationHelper().registeredRoutes;
         String[] currentRouteParts = event.getLocation().getSegments().toArray(new String[]{});
-        Optional<RouteSimilarity> result = routes.entrySet()
-                .stream()
-                .map(routeData -> new RouteSimilarity(routeData.getKey(), currentRouteParts))
-                .max(Comparator.comparingInt(RouteSimilarity::getSimilarity));
+        List<RouteData> availableRoutes = UI.getCurrent().getRouter().getRoutes();
+
+        Optional<RouteSimilarity> result = availableRoutes.stream()
+            .map(ar -> new RouteSimilarity(ar, currentRouteParts))
+            .max(Comparator.comparingInt(RouteSimilarity::getSimilarity));
+
         return result.filter(routeSimilarity -> routeSimilarity.getRoute() == className).isPresent();
-    }
-
-    public static UpNavigationHelper getUpNavigationHelper() {
-        if (UI.getCurrent().getSession().getAttribute(UpNavigationHelper.class) == null) {
-            setUpNavigationHelper();
-        }
-        return UI.getCurrent().getSession().getAttribute(UpNavigationHelper.class);
-    }
-
-    public static void setUpNavigationHelper() {
-        UI.getCurrent().getSession().setAttribute(UpNavigationHelper.class, new UpNavigationHelper());
-    }
-
-    /**
-     * We need to be able to differenciate between routes that have been added to the
-     *
-     * @param className
-     */
-    public static void registerNavigationRoute(Class<? extends Component> className) {
-        getUpNavigationHelper().register(className);
-    }
-
-    public void register(Class<? extends Component> className) {
-        registeredRoutes.put(className, UI.getCurrent().getRouter().getUrl(className));
     }
 }

app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/interfaces/NavigationElementComponent.java
@@ -1,13 +1,0 @@
-package com.github.appreciated.app.layout.builder.interfaces;
-
-import com.vaadin.flow.component.Component;
-
-/**
- * This interface is for Components that contain Elements that are clickable and the interaction with it leads to route
- * changes.
- * This is especially important to highlight navigation elements if the view they are related with was navigated to,
- * without the elements knowledge (f.e. loading the page with a nested url).
- */
-public interface NavigationElementComponent extends NavigationElement {
-    void register();
-}

app-layout-addon/src/main/java/com/github/appreciated/app/layout/builder/interfaces/NavigationElementContainer.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
  */
 public interface NavigationElementContainer extends NavigationElement {
 
-    default void setActiveNavigationElement(NavigationElementComponent active) {
+    default void setActiveNavigationElement(NavigationElement active) {
     }
 
     default void applyParentToItems(Stream<Component> elements) {

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/left/LeftSubmenu.java
@@ -1,7 +1,7 @@
 package com.github.appreciated.app.layout.component.menu.left;
 
 import com.github.appreciated.IronCollapseLayout;
-import com.github.appreciated.app.layout.builder.interfaces.NavigationElementComponent;
+import com.github.appreciated.app.layout.builder.interfaces.NavigationElement;
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElementContainer;
 import com.github.appreciated.app.layout.component.menu.left.items.LeftIconItem;
 import com.vaadin.flow.component.Component;
@@ -70,7 +70,7 @@ public class LeftSubmenu extends Composite<IronCollapseLayout> implements Naviga
     }
 
     @Override
-    public void setActiveNavigationElement(NavigationElementComponent active) {
+    public void setActiveNavigationElement(NavigationElement active) {
         if (active != null) {
             item.getElement().setAttribute("highlight", true);
         } else {

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/top/TopMenuComponent.java
@@ -1,6 +1,6 @@
 package com.github.appreciated.app.layout.component.menu.top;
 
-import com.github.appreciated.app.layout.builder.interfaces.NavigationElementComponent;
+import com.github.appreciated.app.layout.builder.interfaces.NavigationElement;
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElementContainer;
 import com.github.appreciated.app.layout.webcomponents.papertabs.PaperTab;
 import com.github.appreciated.app.layout.webcomponents.papertabs.PaperTabs;
@@ -21,7 +21,7 @@ public class TopMenuComponent extends PaperTabs implements NavigationElementCont
     }
 
     @Override
-    public void setActiveNavigationElement(NavigationElementComponent active) {
+    public void setActiveNavigationElement(NavigationElement active) {
         setSelected((PaperTab) active);
     }
 }

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/top/item/TopNavigationItem.java
@@ -1,6 +1,6 @@
 package com.github.appreciated.app.layout.component.menu.top.item;
 
-import com.github.appreciated.app.layout.builder.interfaces.NavigationElementComponent;
+import com.github.appreciated.app.layout.builder.interfaces.NavigationElement;
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElementContainer;
 import com.github.appreciated.app.layout.webcomponents.papertabs.PaperTab;
 import com.vaadin.flow.component.Component;
@@ -10,7 +10,8 @@ import com.vaadin.flow.component.icon.Icon;
  * A wrapper class for a MenuElement that is clickable and backed by the Navigator. Which means that clicks on instances
  * on {@link TopNavigationItem} respectively their {@link Component} which will usually causes a change of the View at the AppLayout content view.
  */
-public class TopNavigationItem extends PaperTab implements NavigationElementContainer, NavigationElementComponent {
+public class TopNavigationItem extends PaperTab implements NavigationElementContainer,
+    NavigationElement {
 
     private NavigationElementContainer parent;
 
@@ -35,13 +36,9 @@ public class TopNavigationItem extends PaperTab implements NavigationElementCont
     }
 
     @Override
-    public void setActiveNavigationElement(NavigationElementComponent component) {
+    public void setActiveNavigationElement(NavigationElement component) {
         if (parent != null) {
             parent.setActiveNavigationElement(this);
         }
-    }
-
-    @Override
-    public void register() {
     }
 }

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/top/item/TopNavigationLink.java
@@ -1,7 +1,6 @@
 package com.github.appreciated.app.layout.component.menu.top.item;
 
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElement;
-import com.github.appreciated.app.layout.builder.interfaces.NavigationElementComponent;
 import com.github.appreciated.app.layout.builder.interfaces.NavigationElementContainer;
 import com.github.appreciated.app.layout.router.navigation.UpNavigationHelper;
 import com.vaadin.flow.component.Component;
@@ -13,7 +12,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.HighlightAction;
 import com.vaadin.flow.router.RouterLink;
 
-public class TopNavigationLink extends RouterLink implements NavigationElementComponent {
+public class TopNavigationLink extends RouterLink implements NavigationElement {
     private Class<? extends Component> view;
     private NavigationElementContainer parent;
 
@@ -31,7 +30,6 @@ public class TopNavigationLink extends RouterLink implements NavigationElementCo
         wrapper.setHeight("100%");
         add(wrapper);
         setRoute(UI.getCurrent().getRouter(), view);
-        UpNavigationHelper.registerNavigationRoute(view);
         setHighlightCondition((routerLink, event) -> UpNavigationHelper.shouldHighlight(view, event));
         HighlightAction<RouterLink> action = getHighlightAction();
         setHighlightAction((routerLink, highlight) -> {
@@ -46,10 +44,5 @@ public class TopNavigationLink extends RouterLink implements NavigationElementCo
     @Override
     public void setNavigationElementContainer(NavigationElementContainer parent) {
         this.parent = parent;
-    }
-
-    @Override
-    public void register() {
-        UpNavigationHelper.registerNavigationRoute(view);
     }
 }

commit 099a243b1eeb61b1236592b592a86c5832388ebd

brought back UpNavigation but also support route aliases which was broken by #255.

app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/left/items/LeftNavigationItem.java
@@ -45,6 +45,7 @@ public class LeftNavigationItem extends LeftBadgeIconItem implements NavigationE
         this.caption = caption;
         this.icon = icon;
         this.className = className;
+        UpNavigationHelper.registerNavigationRoute(className);
         setRoute(UI.getCurrent().getRouter(), className);
         setHighlightCondition((routerLink, event) -> UpNavigationHelper.shouldHighlight(className, event));
         HighlightAction<RouterLink> action = getHighlightAction();

app-layout-addon/src/main/java/com/github/appreciated/app/layout/router/navigation/UpNavigationHelper.java
@@ -4,11 +4,9 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.RouteData;
-import com.vaadin.flow.router.RouterLink;
 
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,6 +14,8 @@ import java.util.Optional;
  * and helps in finding possible parent views
  */
 public class UpNavigationHelper {
+
+    private HashMap<RouteData, String> registeredRoutes = new HashMap();
 
     private UpNavigationHelper() {
 
@@ -46,13 +46,43 @@ public class UpNavigationHelper {
     }
 
     public static boolean shouldHighlight(Class<? extends Component> className, AfterNavigationEvent event) {
+        HashMap<RouteData, String> routes = getUpNavigationHelper().registeredRoutes;
+
         String[] currentRouteParts = event.getLocation().getSegments().toArray(new String[]{});
-        List<RouteData> availableRoutes = UI.getCurrent().getRouter().getRoutes();
-
-        Optional<RouteSimilarity> result = availableRoutes.stream()
-            .map(ar -> new RouteSimilarity(ar, currentRouteParts))
-            .max(Comparator.comparingInt(RouteSimilarity::getSimilarity));
-
+        Optional<RouteSimilarity> result = routes.entrySet()
+                .stream()
+                .map(routeData -> new RouteSimilarity(routeData.getKey(), currentRouteParts))
+                .max(Comparator.comparingInt(RouteSimilarity::getSimilarity));
         return result.filter(routeSimilarity -> routeSimilarity.getRoute() == className).isPresent();
+    }
+
+    public static UpNavigationHelper getUpNavigationHelper() {
+        if (UI.getCurrent().getSession().getAttribute(UpNavigationHelper.class) == null) {
+            setUpNavigationHelper();
+        }
+        return UI.getCurrent().getSession().getAttribute(UpNavigationHelper.class);
+    }
+
+    public static void setUpNavigationHelper() {
+        UI.getCurrent().getSession().setAttribute(UpNavigationHelper.class, new UpNavigationHelper());
+    }
+
+    /**
+     * We need to be able to differenciate between routes that have been added to the
+     *
+     * @param className
+     */
+    public static void registerNavigationRoute(Class<? extends Component> className) {
+        getUpNavigationHelper().register(className);
+    }
+
+    public void register(Class<? extends Component> className) {
+        UI.getCurrent()
+                .getRouter()
+                .getRoutes()
+                .stream()
+                .filter(routeData -> routeData.getNavigationTarget() == className)
+                .findFirst()
+                .ifPresent(routeData -> registeredRoutes.put(routeData, UI.getCurrent().getRouter().getUrl(className)));
     }
 }