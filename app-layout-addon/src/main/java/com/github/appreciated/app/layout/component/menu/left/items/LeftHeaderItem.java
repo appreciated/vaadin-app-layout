3326d7ac38523c21a21ee139cb8d26b0a5a4bbcf
appreciated <GoebelJohannes@gmx.net>
2019-04-22 13:22:30 +0200

    - improves paddings for menus without header
    (cherry picked from commit c7f71b4732bcfb267d32cb96bf9b9f5add6d4c8f)

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/left/items/LeftHeaderItem.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/component/menu/left/items/LeftHeaderItem.java
@@ -11,9 +11,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class LeftHeaderItem extends Composite<VerticalLayout> {
 
     public LeftHeaderItem(String title, String subtitle, String src) {
-
         VerticalLayout content = getContent();
-
+        content.setPadding(false);
+        content.getStyle().set("padding","var(--app-layout-menu-header-padding)");
         content.setMargin(false);
         setId("menu-header-wrapper");
         if (src != null) {
