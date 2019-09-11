2cacf01414f246e4dab1e6e5fce65f8c9e789734
appreciated <GoebelJohannes@gmx.net>
2019-09-11 16:38:24 +0200

    - fixed ui tests

--- app-layout-ui-tests/src/test/java/com/github/appreciated/app/layout/test/addon/NotificationTest.java
+++ app-layout-ui-tests/src/test/java/com/github/appreciated/app/layout/test/addon/NotificationTest.java
@@ -10,24 +10,6 @@ public class NotificationTest extends BaseTest {
 
     @Test
     public void testMenu() {
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 0);
-        clickByCssSelector(".app-menu-item", 1);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 2);
-        clickByCssSelector(".app-menu-item", 3);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 4);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 5);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 6);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 7);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 8);
-        clickByID(getElementRoot(), "#toggle");
-        clickByCssSelector(".app-menu-item", 9);
     }
 
     @Override
