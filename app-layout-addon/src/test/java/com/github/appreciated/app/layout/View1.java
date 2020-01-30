8933502b7b27cae3183ae510a78ca1eb61c4ae0c
appreciated <GoebelJohannes@gmx.net>
2020-01-30 21:21:50 +0100

    - updated vaadin version und package.json

--- app-layout-addon/src/test/java/com/github/appreciated/app/layout/View1.java
+++ app-layout-addon/src/test/java/com/github/appreciated/app/layout/View1.java
@@ -1,18 +1,62 @@
 package com.github.appreciated.app.layout;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Route(value = "", layout = MainLayout.class)
 public class View1 extends VerticalLayout {
 
     public View1() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        VerticalLayout horizontalLayout = new VerticalLayout();
         horizontalLayout.getStyle().set("border", "1px black solid").set("padding", "10px").set("margin", "0px");
         add(horizontalLayout);
+
+        Tab tab1 = new Tab("Tab one");
+        Div page1 = new Div();
+        page1.setText("Page#1");
+
+        Tab tab2 = new Tab("Tab two");
+        Div page2 = new Div();
+        page2.setText("Page#2");
+        page2.setVisible(false);
+
+        Tab tab3 = new Tab("Tab three");
+        Div page3 = new Div();
+        page3.setText("Page#3");
+        page3.setVisible(false);
+
+        Map<Tab, Component> tabsToPages = new HashMap<>();
+        tabsToPages.put(tab1, page1);
+        tabsToPages.put(tab2, page2);
+        tabsToPages.put(tab3, page3);
+        Tabs tabs = new Tabs(tab1, tab2, tab3);
+        Div pages = new Div(page1, page2, page3);
+
+        Set<Component> pagesShown = Stream.of(page1)
+                .collect(Collectors.toSet());
+
+        tabs.addSelectedChangeListener(event -> {
+            pagesShown.forEach(page -> page.setVisible(false));
+            pagesShown.clear();
+            Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
+            selectedPage.setVisible(true);
+            pagesShown.add(selectedPage);
+        });
+
+        horizontalLayout.add(tabs, pages);
+
         horizontalLayout.add(new Button("Test"), new Checkbox("My Checkbox"));
         setMargin(false);
         setPadding(false);
