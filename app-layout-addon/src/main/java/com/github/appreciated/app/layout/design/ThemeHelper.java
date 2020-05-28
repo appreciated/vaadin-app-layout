4e04050f66fcc49782d658eabc95ffad264d630d
appreciated <GoebelJohannes@gmx.net>
2020-05-28 11:05:32 +0200

    - updated vaadin version - reverted pom changes - added lock file

--- app-layout-addon/src/main/java/com/github/appreciated/app/layout/design/ThemeHelper.java
+++ app-layout-addon/src/main/java/com/github/appreciated/app/layout/design/ThemeHelper.java
@@ -4,7 +4,7 @@ package com.github.appreciated.app.layout.design;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
 
-@StyleSheet(value = "./com/github/appreciated/app-layout/app-layout-styles-material.css")
+@StyleSheet("./com/github/appreciated/app-layout/app-layout-styles-material.css")
 @StyleSheet("./com/github/appreciated/app-layout/app-layout-styles-lumo.css")
 /*
  * Vaadin seem to scan the classes on the production builds for {@link StyleSheet} annotations to pack the production
